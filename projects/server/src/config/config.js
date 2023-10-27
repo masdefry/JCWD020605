337e750fe785f17072c01c0b3e8ee346d86d0819
Ryan Defryan <muhammaddefryan@gmail.com>
2023-10-27 08:33:35 +0700

    Update config.js

--- projects/server/src/config/config.js
+++ projects/server/src/config/config.js
@@ -7,17 +7,17 @@ const { db_username, db_password, db_database, db_host, db_dialect } =
 
 module.exports = {
   development: {
-    username: db_username,
-    password: db_password,
-    database: db_database,
-    host: db_host,
-    dialect: db_dialect,
+    username: process.env.db_username,
+    password: process.env.db_password,
+    database: process.env.db_database,
+    host: process.env.db_host,
+    dialect: process.env.db_dialect,
   },
   production: {
-    username: db_username,
-    password: db_password,
-    database: db_database,
-    host: db_host,
-    dialect: db_dialect,
+    username: process.env.db_username,
+    password: process.env.db_password,
+    database: process.env.db_database,
+    host: process.env.db_host,
+    dialect: process.env.db_dialect,
   },
 };
