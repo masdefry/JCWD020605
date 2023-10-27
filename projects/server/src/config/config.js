962bf9905a9c0d556c5250d505c5c44f6f0e60b3
Ryan Defryan <muhammaddefryan@gmail.com>
2023-10-27 08:57:43 +0700

    Update config.js

--- projects/server/src/config/config.js
+++ projects/server/src/config/config.js
@@ -1,16 +1,28 @@
-{
+require("dotenv").config({ path: `${__dirname}/../.env` });
+
+module.exports = {
   development: {
     username: process.env.db_username,
     password: process.env.db_password,
     database: process.env.db_database,
     host: process.env.db_host,
+    port: process.env.db_port,
     dialect: process.env.db_dialect
+  },
+  test: {
+     username: process.env.db_username,
+     password: process.env.db_password,
+     database: process.env.db_database,
+     host: process.env.db_host,
+     port: process.env.db_port,
+     dialect: process.env.db_dialect
   },
   production: {
     username: process.env.db_username,
     password: process.env.db_password,
     database: process.env.db_database,
     host: process.env.db_host,
+    port: process.env.db_port,
     dialect: process.env.db_dialect
-  }
-}
+  },
+};
